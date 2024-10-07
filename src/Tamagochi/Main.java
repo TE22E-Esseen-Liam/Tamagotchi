e0228ba7603acd04fcae9df8543158d7ee39ea0c
Liam Esseen <Liam.esseen@elev.ga.ntig.se>
2024-10-07 09:52:18 +0200

    Första commiten

---
+++ src/Tamagochi/Main.java
@@ -0,0 +1,8 @@
+package Tamagochi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
---
+++ src/Tamagochi/Tamagotchi.java
@@ -0,0 +1,15 @@
+package Tamagochi;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Tamagotchi {
+
+    int hunger = 100;
+    private int boredom = 0;
+    private ArrayList<String> words = new ArrayList<String>();
+    private boolean isAlive;
+    public String name;
+    Random rand = new Random();
+}
+
